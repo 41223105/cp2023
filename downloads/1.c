e1b62c0a500d4919ff9f10cfde155be9889525f4
41223105 <145425162+41223105@users.noreply.github.com>
2024-01-02 06:56:11 +0000

    ANSIC

---
+++ downloads/1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : 許舒閑\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : October 26, 2004\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 0988-629882\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
